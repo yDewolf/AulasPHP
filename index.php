550b80be476791cd9f24786cd248bcb84db27829
yDewolf <andrersng@gmail.com>
2025-03-19 12:43:45 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 01</title>
+</head>
+<body>
+    <div id="top">
+
+    </div>
+    
+    <div id="menu">
+
+    </div>
+    
+    <div id="content">
+
+    </div>
+
+    <div id="bottom">
+
+    </div>
+</body>
+</html>
