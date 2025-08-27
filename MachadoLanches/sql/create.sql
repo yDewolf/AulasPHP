ce8e466d99f64fdd090bf73aba98f1be3e3d1045
yDewolf <andrersng@gmail.com>
2025-08-27 13:23:01 -0300

    Create Produtos, Mesas and Pedidos tables

--- MachadoLanches/sql/create.sql
+++ MachadoLanches/sql/create.sql
@@ -1,8 +1,10 @@
+DROP DATABASE MachadoLanches;
+
 CREATE DATABASE MachadoLanches;
 USE MachadoLanches;
 
 CREATE TABLE funcionarios (
-    id_funcionario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
     nome_funcionario VARCHAR(255) NOT NULL,
     cargo VARCHAR(255) NOT NULL,
     data_admissao DATE NOT NULL,
@@ -16,4 +18,27 @@ CREATE TABLE clientes (
     endereco VARCHAR(255) NOT NULL,
     sexo ENUM('M', 'F') NOT NULL DEFAULT ('F'),
     data_cadastro DATE NOT NULL
+);
+
+CREATE TABLE produtos (
+    id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    preco_unit DECIMAL(4,2) NOT NULL,
+    nome VARCHAR(127) NOT NULL,
+    descricao VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE mesas (
+    id_mesa INT PRIMARY KEY AUTO_INCREMENT,
+    quantidade_pessoas INT NOT NULL,
+    horario_entrada DATETIME DEFAULT CURRENT_DATETIME,
+    horario_saida DATETIME,
+    vazia BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE pedidos (
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    id_mesa INT NOT NULL,
+    id_produto INT NOT NULL,
+    FOREIGN KEY (id_mesa) REFERENCES mesas(id_mesa),
+    FOREIGN KEY (id_pedido) REFERENCES produtos(id_produto)
 );
