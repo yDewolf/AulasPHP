8a49a698e639974a17316e7117abc6d388bbadcb
yDewolf <andrersng@gmail.com>
2025-09-03 13:28:37 -0300

    Aula 01 Tocador de músicas

---
+++ player-de-musicas/readme.md
@@ -0,0 +1,1 @@
+## Ref: [Aula 01 pdf](https://etecspgov.sharepoint.com/:b:/s/Section_11207.A.GB.I.495.031.20250/EeqNa8FbmC5Jhylp6gzLPzABTIxbJZ0zslkF4mJD84du4w?e=RhNU20)
---
+++ player-de-musicas/sql/create.sql
@@ -0,0 +1,14 @@
+-- DROP DATABASE music_player;
+CREATE DATABASE music_player;
+USE music_player;
+
+CREATE TABLE songs (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    artista VARCHAR(255) NOT NULL,
+    album VARCHAR(255),
+    duracao INT(11),
+    caminho_arquivo VARCHAR(255) NOT NULL,
+    data_adicao DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
